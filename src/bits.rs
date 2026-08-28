209d150c825181e598294481be2c650d1cbcc1d2
Nodo1221 <killforskill2005@gmail.com>
2026-08-28 20:16:42 +0200

    wip: introduced pagefull newtype

--- src/bits.rs
+++ src/bits.rs
@@ -3,9 +3,11 @@ use std::fmt;
 pub type Block = u8;
 pub const PAGE_SIZE: usize = 1024;
 
+pub struct PageFull(pub bool);
+
 pub struct BitData {
     pub data: [Block; PAGE_SIZE],
-    pub index: usize,
+    pub size: usize,
     pub capacity: u8,
     buffer: Block,
 }
@@ -14,20 +16,20 @@ impl BitData {
     pub fn new() -> Self {
         Self {
             data: [0; PAGE_SIZE],
-            index: 0,
+            size: 0,
             capacity: Block::BITS as u8,
             buffer: 0,
         }
     }
 
-    pub fn write(&mut self, mut byte: u32, mut len: u8) -> bool {
+    pub fn write(&mut self, mut byte: u32, mut len: u8) -> PageFull {
         let b = Block::BITS as u8;
 
         // Worst-case space check for a 32-bit write,
         // reserving at least 1 slot for a future flush().
         let max_blocks = 32 / Block::BITS as usize;
-        if self.index + max_blocks >= PAGE_SIZE {
-            return false;
+        if self.size + max_blocks >= PAGE_SIZE {
+            return PageFull(true);
         }
 
         let first = (byte >> (32 - self.capacity)) as Block;
@@ -35,11 +37,11 @@ impl BitData {
         if len < self.capacity {
             self.buffer |= first;
             self.capacity -= len;
-            return true;
+            return PageFull(false);
         }
 
-        self.data[self.index] = self.buffer | first;
-        self.index += 1;
+        self.data[self.size] = self.buffer | first;
+        self.size += 1;
 
         byte <<= self.capacity;
         len -= self.capacity;
@@ -48,20 +50,20 @@ impl BitData {
 
         for i in 0..chunks {
             let current = (byte >> (32 - b - i * b)) as Block;
-            self.data[self.index] = current;
-            self.index += 1;
+            self.data[self.size] = current;
+            self.size += 1;
         }
 
         let last = (byte >> (32 - b - chunks * b)) as Block;
         self.buffer = last;
         self.capacity = b - (len - b * chunks);
 
-        true
+        PageFull(false)
     }
 
     pub fn flush(&mut self) {
-        self.data[self.index] = self.buffer;
-        self.index += 1;
+        self.data[self.size] = self.buffer;
+        self.size += 1;
         self.buffer = 0;
     }
 }
@@ -69,8 +71,7 @@ impl BitData {
 impl fmt::Display for BitData {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let width = Block::BITS as usize;
-        // Only iterate up to the current index
-        for i in 0..self.index {
+        for i in 0..self.size {
             writeln!(f, "{:0width$b}", self.data[i], width = width)?;
         }
         write!(f, "current capacity: {}", self.capacity)
